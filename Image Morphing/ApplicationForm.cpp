c44b25a9c236037be248db5931b4517780520b4e
tobygameac <tobyreallygameac@gmail.com>
2016-03-09 17:03:40 +0800

    basic interface

--- Image Morphing/ApplicationForm.cpp
+++ Image Morphing/ApplicationForm.cpp
@@ -1,2 +1,73 @@
 #include "ApplicationForm.h"
 
+namespace ImageMorphing {
+
+  [STAThread]
+  int main(array<String^> ^args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    ImageMorphing::ApplicationForm form;
+    Application::Run(%form);
+    return 0;
+  }
+
+  ApplicationForm::ApplicationForm() {
+    InitializeComponent();
+
+    open_source_image_tool_strip_menu_item_->Click += gcnew System::EventHandler(this, &ImageMorphing::ApplicationForm::OnButtonsClick);
+    open_destination_image_tool_strip_menu_item_->Click += gcnew System::EventHandler(this, &ImageMorphing::ApplicationForm::OnButtonsClick);
+
+    Test();
+  }
+
+  ApplicationForm::~ApplicationForm() {
+    if (components) {
+      delete components;
+    }
+  }
+
+  void ApplicationForm::OnButtonsClick(System::Object ^sender, System::EventArgs ^e) {
+    if (sender == open_source_image_tool_strip_menu_item_ || sender == open_destination_image_tool_strip_menu_item_) {
+      OpenFileDialog ^open_image_file_dialog = gcnew OpenFileDialog();
+      open_image_file_dialog->Filter = "Image files | *.*";
+      open_image_file_dialog->Title = "Open an image file.";
+
+      if (open_image_file_dialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+        Bitmap ^source_bitmap = gcnew Bitmap(open_image_file_dialog->FileName);
+        (sender == open_source_image_tool_strip_menu_item_ ? source_picture_box_ : destination_picture_box_)->Image = source_bitmap;
+
+        std::string raw_file_path = msclr::interop::marshal_as<std::string>(open_image_file_dialog->FileName);
+        (sender == open_source_image_tool_strip_menu_item_ ? original_source_image : original_destination_image) = cv::imread(raw_file_path);
+
+        AdjustImagesSize();
+      }
+    }
+  }
+
+  void ApplicationForm::AdjustImagesSize() {
+    if (original_source_image.empty() || original_destination_image.empty()) {
+      return;
+    }
+
+    const size_t ORIGINAL_SOURCE_IMAGE_AREA = (size_t)original_source_image.rows * (size_t)original_source_image.cols;
+    const size_t ORIGINAL_DESTINATION_IMAGE_AREA = (size_t)original_destination_image.rows * (size_t)original_destination_image.cols;
+    const cv::Size RESIZED_IMAGE_SIZE = (ORIGINAL_SOURCE_IMAGE_AREA >= ORIGINAL_DESTINATION_IMAGE_AREA) ? original_source_image.size() : original_destination_image.size();
+    cv::resize(original_source_image, resized_source_image, RESIZED_IMAGE_SIZE);
+    cv::resize(original_destination_image, resized_destination_image, RESIZED_IMAGE_SIZE);
+
+    source_picture_box_->Image = CVMatToBitmap(resized_source_image);
+    destination_picture_box_->Image = CVMatToBitmap(resized_destination_image);
+
+    destination_picture_box_->Location = System::Drawing::Point(source_picture_box_->Location.X + resized_source_image.cols + PICTURE_BOX_LOCATION_GAP, source_picture_box_->Location.Y);
+  }
+
+  System::Drawing::Bitmap ^ApplicationForm::CVMatToBitmap(const cv::Mat &mat) {
+    return gcnew System::Drawing::Bitmap(mat.cols, mat.rows, mat.step, System::Drawing::Imaging::PixelFormat::Format24bppRgb, (System::IntPtr)mat.data);
+  }
+
+  void ApplicationForm::Test() {
+    original_source_image = cv::imread("..//data//butterfly.jpg");
+    original_destination_image = cv::imread("..//data//butterfly2.jpg");
+    AdjustImagesSize();
+  }
+}
--- Image Morphing/ApplicationForm.h
+++ Image Morphing/ApplicationForm.h
@@ -1,59 +1,167 @@
 #pragma once
 
+#include <opencv\cv.hpp>
+
+#include <msclr\marshal_cppstd.h>
+#using <mscorlib.dll>
+
 namespace ImageMorphing {
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+  cv::Mat original_source_image;
+  cv::Mat original_destination_image;
+  cv::Mat resized_source_image;
+  cv::Mat resized_destination_image;
 
-	/// <summary>
-	/// Summary for ApplicationForm
-	/// </summary>
-	public ref class ApplicationForm : public System::Windows::Forms::Form
-	{
-	public:
-		ApplicationForm(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
+  const size_t PICTURE_BOX_LOCATION_GAP = 50;
 
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~ApplicationForm()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+  using namespace System;
+  using namespace System::ComponentModel;
+  using namespace System::Collections;
+  using namespace System::Windows::Forms;
+  using namespace System::Data;
+  using namespace System::Drawing;
 
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+  /// <summary>
+  /// Summary for ApplicationForm
+  /// </summary>
+  public ref class ApplicationForm : public System::Windows::Forms::Form {
+
+  public:
+
+    ApplicationForm();
+
+  private:
+
+    void OnButtonsClick(System::Object ^sender, System::EventArgs ^e);
+
+    void AdjustImagesSize();
+
+    System::Drawing::Bitmap ^CVMatToBitmap(const cv::Mat &mat);
+
+    void Test();
+
+  protected:
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    ~ApplicationForm();
+
+  private:
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    System::ComponentModel::Container ^components;
+    System::Windows::Forms::Panel ^picture_box_panel_;
+    System::Windows::Forms::PictureBox ^destination_picture_box_;
+    System::Windows::Forms::PictureBox ^source_picture_box_;
+    System::Windows::Forms::MenuStrip ^files_menu_strip_;
+    System::Windows::Forms::ToolStripMenuItem ^files_tool_strip_menu_item_;
+    System::Windows::Forms::ToolStripMenuItem ^open_source_image_tool_strip_menu_item_;
+    System::Windows::Forms::ToolStripMenuItem ^open_destination_image_tool_strip_menu_item_;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"ApplicationForm";
-			this->Padding = System::Windows::Forms::Padding(0);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-		}
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    void InitializeComponent(void) {
+      this->picture_box_panel_ = (gcnew System::Windows::Forms::Panel());
+      this->destination_picture_box_ = (gcnew System::Windows::Forms::PictureBox());
+      this->source_picture_box_ = (gcnew System::Windows::Forms::PictureBox());
+      this->files_menu_strip_ = (gcnew System::Windows::Forms::MenuStrip());
+      this->files_tool_strip_menu_item_ = (gcnew System::Windows::Forms::ToolStripMenuItem());
+      this->open_source_image_tool_strip_menu_item_ = (gcnew System::Windows::Forms::ToolStripMenuItem());
+      this->open_destination_image_tool_strip_menu_item_ = (gcnew System::Windows::Forms::ToolStripMenuItem());
+      this->picture_box_panel_->SuspendLayout();
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->destination_picture_box_))->BeginInit();
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->source_picture_box_))->BeginInit();
+      this->files_menu_strip_->SuspendLayout();
+      this->SuspendLayout();
+      // 
+      // picture_box_panel_
+      // 
+      this->picture_box_panel_->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+        | System::Windows::Forms::AnchorStyles::Left)
+        | System::Windows::Forms::AnchorStyles::Right));
+      this->picture_box_panel_->AutoScroll = true;
+      this->picture_box_panel_->Controls->Add(this->destination_picture_box_);
+      this->picture_box_panel_->Controls->Add(this->source_picture_box_);
+      this->picture_box_panel_->Location = System::Drawing::Point(69, 63);
+      this->picture_box_panel_->Name = L"picture_box_panel_";
+      this->picture_box_panel_->Size = System::Drawing::Size(1190, 495);
+      this->picture_box_panel_->TabIndex = 0;
+      // 
+      // destination_picture_box_
+      // 
+      this->destination_picture_box_->Location = System::Drawing::Point(111, 4);
+      this->destination_picture_box_->Name = L"destination_picture_box_";
+      this->destination_picture_box_->Size = System::Drawing::Size(100, 50);
+      this->destination_picture_box_->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+      this->destination_picture_box_->TabIndex = 1;
+      this->destination_picture_box_->TabStop = false;
+      // 
+      // source_picture_box_
+      // 
+      this->source_picture_box_->Location = System::Drawing::Point(4, 4);
+      this->source_picture_box_->Name = L"source_picture_box_";
+      this->source_picture_box_->Size = System::Drawing::Size(100, 50);
+      this->source_picture_box_->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+      this->source_picture_box_->TabIndex = 0;
+      this->source_picture_box_->TabStop = false;
+      // 
+      // files_menu_strip_
+      // 
+      this->files_menu_strip_->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {
+        this->files_tool_strip_menu_item_
+      });
+      this->files_menu_strip_->Location = System::Drawing::Point(0, 0);
+      this->files_menu_strip_->Name = L"files_menu_strip_";
+      this->files_menu_strip_->Size = System::Drawing::Size(1271, 24);
+      this->files_menu_strip_->TabIndex = 1;
+      this->files_menu_strip_->Text = L"Menu";
+      // 
+      // files_tool_strip_menu_item_
+      // 
+      this->files_tool_strip_menu_item_->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+        this->open_source_image_tool_strip_menu_item_,
+          this->open_destination_image_tool_strip_menu_item_
+      });
+      this->files_tool_strip_menu_item_->Name = L"files_tool_strip_menu_item_";
+      this->files_tool_strip_menu_item_->Size = System::Drawing::Size(42, 20);
+      this->files_tool_strip_menu_item_->Text = L"Files";
+      // 
+      // open_source_image_tool_strip_menu_item_
+      // 
+      this->open_source_image_tool_strip_menu_item_->Name = L"open_source_image_tool_strip_menu_item_";
+      this->open_source_image_tool_strip_menu_item_->Size = System::Drawing::Size(201, 22);
+      this->open_source_image_tool_strip_menu_item_->Text = L"Open source image";
+      // 
+      // open_destination_image_tool_strip_menu_item_
+      // 
+      this->open_destination_image_tool_strip_menu_item_->Name = L"open_destination_image_tool_strip_menu_item_";
+      this->open_destination_image_tool_strip_menu_item_->Size = System::Drawing::Size(201, 22);
+      this->open_destination_image_tool_strip_menu_item_->Text = L"Open destination image";
+      // 
+      // ApplicationForm
+      // 
+      this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+      this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+      this->ClientSize = System::Drawing::Size(1271, 570);
+      this->Controls->Add(this->picture_box_panel_);
+      this->Controls->Add(this->files_menu_strip_);
+      this->MainMenuStrip = this->files_menu_strip_;
+      this->Name = L"ApplicationForm";
+      this->Text = L"ApplicationForm";
+      this->picture_box_panel_->ResumeLayout(false);
+      this->picture_box_panel_->PerformLayout();
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->destination_picture_box_))->EndInit();
+      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->source_picture_box_))->EndInit();
+      this->files_menu_strip_->ResumeLayout(false);
+      this->files_menu_strip_->PerformLayout();
+      this->ResumeLayout(false);
+      this->PerformLayout();
+
+    }
 #pragma endregion
-	};
+  };
 }
