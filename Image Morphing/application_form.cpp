83b70494be25da396bcee998348c42e0e9905291
tobygameac <tobyreallygameac@gmail.com>
2016-04-12 23:08:34 +0800

    multiple images

---
+++ Image Morphing/application_form.cpp
@@ -0,0 +1,353 @@
+#include "application_form.h"
+
+namespace ImageMorphing {
+
+  [STAThread]
+  int main(array<String^> ^args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    ImageMorphing::ApplicationForm form;
+    Application::Run(%form);
+    return 0;
+  }
+
+  ApplicationForm::ApplicationForm() {
+    InitializeComponent();
+
+    picture_boxes = gcnew System::Collections::Generic::List<System::Windows::Forms::PictureBox ^>();
+
+    add_image_tool_strip_menu_item_->Click += gcnew System::EventHandler(this, &ImageMorphing::ApplicationForm::OnButtonsClick);
+
+    start_button_->Click += gcnew System::EventHandler(this, &ImageMorphing::ApplicationForm::OnButtonsClick);
+    clear_features_button_->Click += gcnew System::EventHandler(this, &ImageMorphing::ApplicationForm::OnButtonsClick);
+    load_features_button_->Click += gcnew System::EventHandler(this, &ImageMorphing::ApplicationForm::OnButtonsClick);
+    save_features_button_->Click += gcnew System::EventHandler(this, &ImageMorphing::ApplicationForm::OnButtonsClick);
+
+    srand(time(0));
+
+    //Test();
+  }
+
+  ApplicationForm::~ApplicationForm() {
+    if (components) {
+      delete components;
+    }
+  }
+
+  void ApplicationForm::OnButtonsClick(System::Object ^sender, System::EventArgs ^e) {
+    if (sender == add_image_tool_strip_menu_item_) {
+      OpenFileDialog ^open_image_file_dialog = gcnew OpenFileDialog();
+      open_image_file_dialog->Filter = "Image files | *.*";
+      open_image_file_dialog->Title = "Open an image file.";
+
+      if (open_image_file_dialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+        System::Windows::Forms::PictureBox ^new_picture_box = gcnew System::Windows::Forms::PictureBox();
+
+        new_picture_box->Location = System::Drawing::Point(200, 4);
+        new_picture_box->Size = System::Drawing::Size(100, 50);
+        new_picture_box->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+        new_picture_box->TabStop = false;
+
+        new_picture_box->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &ImageMorphing::ApplicationForm::OnMouseMove);
+        new_picture_box->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &ImageMorphing::ApplicationForm::OnMouseDown);
+
+        this->picture_box_panel_->Controls->Add(new_picture_box);
+
+        picture_boxes->Add(new_picture_box);
+
+        source_images.resize(picture_boxes->Count);
+        resized_images.resize(picture_boxes->Count);
+        images_with_feature_lines.resize(picture_boxes->Count);
+        feature_lines_of_images.resize(picture_boxes->Count);
+        last_feature_line_of_images.resize(picture_boxes->Count);
+        is_drawing_feature_of_images.resize(picture_boxes->Count);
+
+        Bitmap ^source_bitmap = gcnew Bitmap(open_image_file_dialog->FileName);
+        new_picture_box->Image = source_bitmap;
+
+        std::string raw_file_path = msclr::interop::marshal_as<std::string>(open_image_file_dialog->FileName);
+        source_images[source_images.size() - 1] = cv::imread(raw_file_path);
+
+        if (picture_boxes->Count >= 2) {
+          start_button_->Enabled = true;
+        }
+
+        AdjustImagesSize();
+      }
+    } else if (sender == start_button_) {
+
+      if (picture_boxes->Count < 2) {
+        return;
+      }
+
+      SaveFileDialog ^save_video_file_dialog = gcnew SaveFileDialog();
+      save_video_file_dialog->Filter = "Video file | *.avi";
+      save_video_file_dialog->Title = "Save a video file.";
+
+      if (save_video_file_dialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+        std::string raw_file_path = msclr::interop::marshal_as<std::string>(save_video_file_dialog->FileName);
+
+        std::vector<std::pair<cv::Point2d, cv::Point2d> > &max_amount_features_lines = feature_lines_of_images[0];
+
+        for (const auto &features_lines : feature_lines_of_images) {
+          if (features_lines.size() > max_amount_features_lines.size()) {
+            max_amount_features_lines = features_lines;
+          }
+        }
+
+        for (auto &features_lines : feature_lines_of_images) {
+          for (size_t i = features_lines.size(); i < max_amount_features_lines.size(); ++i) {
+            features_lines.push_back(max_amount_features_lines[i]);
+          }
+        }
+
+        const double FPS = 30;
+
+        cv::VideoWriter result_video_writer;
+
+        result_video_writer.open(raw_file_path, 0, FPS, resized_images[0].size());
+
+        for (size_t image_index = 1; image_index < source_images.size(); ++image_index) {
+          size_t frame_count = System::Decimal::ToInt32(morphing_steps_numeric_up_down_->Value);
+
+          double t_gap = 1.0 / (double)frame_count;
+
+          std::vector<cv::Mat> result_at_t(frame_count + 1);
+
+#pragma omp parallel for
+          for (int i = 0; i < result_at_t.size(); ++i) {
+            double t = t_gap * i;
+            result_at_t[i] = Morphing(resized_images[image_index - 1], resized_images[image_index],
+              t, feature_lines_of_images[image_index - 1], feature_lines_of_images[image_index], 1, 2, 0).clone();
+
+            std::cout << "Done : " << image_index << " - " << t << "\n";
+          }
+
+          for (const cv::Mat &frame : result_at_t) {
+            result_video_writer.write(frame);
+          }
+        }
+
+        //for (double t = 0; t <= 1; t += t_gap) {
+        //  cv::Mat result_at_t = Morphing(resized_source_image, resized_destination_image, t, source_feature_lines, destination_feature_lines, 1, 2, 0);
+
+        //  result_video_writer.write(result_at_t);
+        //}
+
+        std::cout << "Done.\n";
+      }
+
+    } else if (sender == clear_features_button_) {
+      feature_lines_of_images.clear();
+
+      for (size_t &is_drawing_feature : is_drawing_feature_of_images) {
+        is_drawing_feature = false;
+      }
+
+      PaintPictureBoxWithFeatures();
+    } else if (sender == load_features_button_) {
+      OpenFileDialog ^open_features_file_dialog = gcnew OpenFileDialog();
+      open_features_file_dialog->Filter = "Features file | *.*";
+      open_features_file_dialog->Title = "Open a features file.";
+
+      if (open_features_file_dialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+        std::string raw_file_path = msclr::interop::marshal_as<std::string>(open_features_file_dialog->FileName);
+        LoadFeatures(raw_file_path);
+      }
+    } else if (sender == save_features_button_) {
+      SaveFileDialog ^save_features_file_dialog = gcnew SaveFileDialog();
+      save_features_file_dialog->Filter = "Features file | *.*";
+      save_features_file_dialog->Title = "Save a features file.";
+
+      if (save_features_file_dialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+        std::string raw_file_path = msclr::interop::marshal_as<std::string>(save_features_file_dialog->FileName);
+        SaveFeatures(raw_file_path);
+      }
+    }
+  }
+
+  void ApplicationForm::OnMouseMove(System::Object ^sender, System::Windows::Forms::MouseEventArgs ^e) {
+    for (size_t i = 0; i < picture_boxes->Count; ++i) {
+      if (sender == picture_boxes[i]) {
+        if (is_drawing_feature_of_images[i]) {
+          last_feature_line_of_images[i].second = cv::Point2d(e->X, e->Y);
+          PaintPictureBoxWithFeatures();
+        }
+        break;
+      }
+    }
+  }
+
+  void ApplicationForm::OnMouseDown(System::Object ^sender, System::Windows::Forms::MouseEventArgs ^e) {
+    if (e->Button == System::Windows::Forms::MouseButtons::Left) {
+      // New features
+      for (size_t i = 0; i < picture_boxes->Count; ++i) {
+        if (sender == picture_boxes[i]) {
+          if (is_drawing_feature_of_images[i]) {
+            last_feature_line_of_images[i].second = cv::Point2d(e->X, e->Y);
+            feature_lines_of_images[i].push_back(last_feature_line_of_images[i]);
+            PaintPictureBoxWithFeatures();
+          } else {
+            last_feature_line_of_images[i].first = cv::Point2d(e->X, e->Y);
+          }
+          is_drawing_feature_of_images[i] = !is_drawing_feature_of_images[i];
+          break;
+        }
+      }
+    } else if (e->Button == System::Windows::Forms::MouseButtons::Right) {
+      // Remove the last feature
+      for (size_t i = 0; i < picture_boxes->Count; ++i) {
+        if (sender == picture_boxes[i]) {
+          if (!is_drawing_feature_of_images[i]) {
+            if (feature_lines_of_images[i].size()) {
+              feature_lines_of_images[i].pop_back();
+            }
+          }
+          is_drawing_feature_of_images[i] = false;
+          PaintPictureBoxWithFeatures();
+          break;
+        }
+      }
+    }
+  }
+
+  void ApplicationForm::AdjustImagesSize() {
+    if (!picture_boxes->Count) {
+      return;
+    }
+
+    start_button_->Enabled = true;
+
+    cv::Size &min_size = source_images[0].size();
+
+    for (const cv::Mat &image : source_images) {
+      min_size.width = std::min(min_size.width, image.size().width);
+      min_size.height = std::min(min_size.height, image.size().height);
+    }
+
+    if (resized_images.size() < source_images.size()) {
+      resized_images.resize(source_images.size());
+    }
+
+    for (size_t i = 0; i < source_images.size(); ++i) {
+      resized_images[i] = source_images[i](cv::Rect(0, 0, min_size.width, min_size.height)).clone();
+    }
+
+    for (size_t i = 0; i < picture_boxes->Count; ++i) {
+      if (!i) {
+        picture_boxes[i]->Location = System::Drawing::Point(picture_box_panel_->Location.X + PICTURE_BOX_LOCATION_GAP, picture_box_panel_->Location.Y + PICTURE_BOX_LOCATION_GAP);
+      } else {
+        picture_boxes[i]->Location = System::Drawing::Point(picture_boxes[i - 1]->Location.X + min_size.width + PICTURE_BOX_LOCATION_GAP, picture_boxes[i - 1]->Location.Y);
+      }
+    }
+
+    PaintPictureBoxWithFeatures();
+  }
+
+  void ApplicationForm::PaintPictureBoxWithFeatures() {
+
+    if (!resized_images.size()) {
+      return;
+    }
+
+    if (images_with_feature_lines.size() != resized_images.size()) {
+      images_with_feature_lines.resize(resized_images.size());
+    }
+
+    size_t max_feature_lines_count = 0;
+
+    for (size_t i = 0; i < images_with_feature_lines.size(); ++i) {
+      images_with_feature_lines[i] = resized_images[i].clone();
+      max_feature_lines_count = std::max(max_feature_lines_count, feature_lines_of_images[i].size());
+    }
+
+    for (size_t i = feature_colors.size(); i < max_feature_lines_count + 1; ++i) {
+      feature_colors.push_back(cv::Vec3b(rand() % 256, rand() % 256, rand() % 256));
+    }
+
+    for (size_t i = 0; i < images_with_feature_lines.size(); ++i) {
+      if (is_drawing_feature_of_images[i]) {
+        cv::line(images_with_feature_lines[i], last_feature_line_of_images[i].first, last_feature_line_of_images[i].second, feature_colors[feature_lines_of_images[i].size()], FEATURE_LINE_THICKNESS);
+      }
+    }
+
+    for (size_t i = 0; i < images_with_feature_lines.size(); ++i) {
+      for (size_t j = 0; j < feature_lines_of_images[i].size(); ++j) {
+        const auto &feature_line = feature_lines_of_images[i][j];
+        cv::line(images_with_feature_lines[i], feature_line.first, feature_line.second, feature_colors[j], FEATURE_LINE_THICKNESS);
+      }
+      picture_boxes[i]->Image = CVMatToBitmap(images_with_feature_lines[i]);
+    }
+  }
+
+  System::Drawing::Bitmap ^ApplicationForm::CVMatToBitmap(const cv::Mat &mat) {
+    //return gcnew System::Drawing::Bitmap(mat.cols, mat.rows, mat.step, System::Drawing::Imaging::PixelFormat::Format24bppRgb, (System::IntPtr)mat.bitmap_data);
+
+    if (mat.type() != CV_8UC3) {
+      throw gcnew NotSupportedException("Only images of type CV_8UC3 are supported for conversion to Bitmap");
+    }
+
+    // Create the bitmap and get the pointer to the bitmap
+    System::Drawing::Imaging::PixelFormat pixel_format(System::Drawing::Imaging::PixelFormat::Format24bppRgb);
+
+    System::Drawing::Bitmap ^bitmap = gcnew System::Drawing::Bitmap(mat.cols, mat.rows, pixel_format);
+
+    System::Drawing::Imaging::BitmapData ^bitmap_data = bitmap->LockBits(System::Drawing::Rectangle(0, 0, mat.cols, mat.rows), System::Drawing::Imaging::ImageLockMode::WriteOnly, pixel_format);
+
+    unsigned char *source_pointer = mat.data;
+
+    unsigned char *destination_pointer = reinterpret_cast<unsigned char*>(bitmap_data->Scan0.ToPointer());
+
+    for (int r = 0; r < bitmap_data->Height; ++r) {
+      memcpy(reinterpret_cast<void *>(&destination_pointer[r * bitmap_data->Stride]), reinterpret_cast<void *>(&source_pointer[r * mat.step]), mat.cols * mat.channels());
+    }
+
+    bitmap->UnlockBits(bitmap_data);
+
+    return bitmap;
+  }
+
+  void ApplicationForm::LoadFeatures(const std::string &file_path) {
+    if (feature_lines_of_images.size() < picture_boxes->Count) {
+      feature_lines_of_images.resize(picture_boxes->Count);
+    }
+
+    for (auto &feature_lines : feature_lines_of_images) {
+      feature_lines.clear();
+    }
+
+    std::ifstream features_input_stream(file_path);
+
+    std::pair<cv::Point2d, cv::Point2d> feature_line;
+
+    size_t target_index = 0;
+
+    while (features_input_stream >> feature_line.first.x >> feature_line.first.y >> feature_line.second.x >> feature_line.second.y) {
+      if (feature_line.first.x < 0) {
+        ++target_index;
+        continue;
+      }
+      feature_lines_of_images[target_index].push_back(feature_line);
+    }
+
+    PaintPictureBoxWithFeatures();
+  }
+
+  void ApplicationForm::SaveFeatures(const std::string &file_path) {
+    std::ofstream features_output_stream(file_path);
+
+    for (auto feature_lines : feature_lines_of_images) {
+      for (auto feature_line : feature_lines) {
+        features_output_stream << feature_line.first.x << " " << feature_line.first.y << " " << feature_line.second.x << " " << feature_line.second.y << "\n";
+      }
+      features_output_stream << "-1 -1 -1 -1\n";
+    }
+  }
+
+  void ApplicationForm::Test() {
+    //original_source_image = cv::imread("..//data//image038.jpg");
+    //original_destination_image = cv::imread("..//data//image036.jpg");
+    //AdjustImagesSize();
+    //LoadFeatures("..//data//features.txt");
+  }
+}
--- Image Morphing/application_form.h
+++ Image Morphing/application_form.h
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include <opencv\cv.hpp>
 
@@ -16,24 +17,17 @@
 
 namespace ImageMorphing {
 
-  cv::Mat original_source_image;
-  cv::Mat original_destination_image;
-  cv::Mat resized_source_image;
-  cv::Mat resized_destination_image;
+  std::vector<cv::Mat> source_images;
+  std::vector<cv::Mat> resized_images;
+  std::vector<cv::Mat> images_with_feature_lines;
 
-  cv::Mat source_features_image;
-  cv::Mat destination_features_image;
+  std::vector<std::vector<std::pair<cv::Point2d, cv::Point2d> > > feature_lines_of_images;
 
-  std::vector<std::pair<cv::Point2d, cv::Point2d> > source_feature_lines;
-  std::vector<std::pair<cv::Point2d, cv::Point2d> > destination_feature_lines;
-
-  std::pair<cv::Point2d, cv::Point2d> last_source_feature_line;
-  std::pair<cv::Point2d, cv::Point2d> last_destination_feature_line;
+  std::vector<std::pair<cv::Point2d, cv::Point2d> > last_feature_line_of_images;
 
   std::vector<cv::Vec3b> feature_colors;
 
-  bool is_drawing_source_features;
-  bool is_drawing_destination_features;
+  std::vector<size_t> is_drawing_feature_of_images;
 
   const size_t FEATURE_LINE_THICKNESS = 3;
   const size_t PICTURE_BOX_LOCATION_GAP = 50;
@@ -56,6 +50,8 @@ namespace ImageMorphing {
 
   private:
 
+    System::Collections::Generic::List<System::Windows::Forms::PictureBox ^> ^picture_boxes;
+
     void OnButtonsClick(System::Object ^sender, System::EventArgs ^e);
 
     void OnMouseMove(System::Object ^sender, System::Windows::Forms::MouseEventArgs ^e);
@@ -67,10 +63,10 @@ namespace ImageMorphing {
     void PaintPictureBoxWithFeatures();
 
     System::Drawing::Bitmap ^CVMatToBitmap(const cv::Mat &mat);
-    
-    void LoadFeatures();
 
-    void SaveFeatures();
+    void LoadFeatures(const std::string &file_path);
+
+    void SaveFeatures(const std::string &file_path);
 
     void Test();
 
@@ -86,17 +82,17 @@ namespace ImageMorphing {
     /// </summary>
     System::ComponentModel::Container ^components;
     System::Windows::Forms::Panel ^picture_box_panel_;
-    System::Windows::Forms::PictureBox ^destination_picture_box_;
-    System::Windows::Forms::PictureBox ^source_picture_box_;
+
     System::Windows::Forms::MenuStrip ^files_menu_strip_;
     System::Windows::Forms::ToolStripMenuItem ^files_tool_strip_menu_item_;
-    System::Windows::Forms::ToolStripMenuItem ^open_source_image_tool_strip_menu_item_;
+    System::Windows::Forms::ToolStripMenuItem^  add_image_tool_strip_menu_item_;
+
     System::Windows::Forms::Button ^start_button_;
     System::Windows::Forms::NumericUpDown ^morphing_steps_numeric_up_down_;
     System::Windows::Forms::Button ^clear_features_button_;
     System::Windows::Forms::Button ^load_features_button_;
     System::Windows::Forms::Button ^save_features_button_;
-    System::Windows::Forms::ToolStripMenuItem ^open_destination_image_tool_strip_menu_item_;
+
 
 #pragma region Windows Form Designer generated code
     /// <summary>
@@ -105,20 +101,14 @@ namespace ImageMorphing {
     /// </summary>
     void InitializeComponent(void) {
       this->picture_box_panel_ = (gcnew System::Windows::Forms::Panel());
-      this->destination_picture_box_ = (gcnew System::Windows::Forms::PictureBox());
-      this->source_picture_box_ = (gcnew System::Windows::Forms::PictureBox());
       this->files_menu_strip_ = (gcnew System::Windows::Forms::MenuStrip());
       this->files_tool_strip_menu_item_ = (gcnew System::Windows::Forms::ToolStripMenuItem());
-      this->open_source_image_tool_strip_menu_item_ = (gcnew System::Windows::Forms::ToolStripMenuItem());
-      this->open_destination_image_tool_strip_menu_item_ = (gcnew System::Windows::Forms::ToolStripMenuItem());
+      this->add_image_tool_strip_menu_item_ = (gcnew System::Windows::Forms::ToolStripMenuItem());
       this->start_button_ = (gcnew System::Windows::Forms::Button());
       this->morphing_steps_numeric_up_down_ = (gcnew System::Windows::Forms::NumericUpDown());
       this->clear_features_button_ = (gcnew System::Windows::Forms::Button());
       this->load_features_button_ = (gcnew System::Windows::Forms::Button());
       this->save_features_button_ = (gcnew System::Windows::Forms::Button());
-      this->picture_box_panel_->SuspendLayout();
-      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->destination_picture_box_))->BeginInit();
-      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->source_picture_box_))->BeginInit();
       this->files_menu_strip_->SuspendLayout();
       (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->morphing_steps_numeric_up_down_))->BeginInit();
       this->SuspendLayout();
@@ -129,30 +119,10 @@ namespace ImageMorphing {
         | System::Windows::Forms::AnchorStyles::Left)
         | System::Windows::Forms::AnchorStyles::Right));
       this->picture_box_panel_->AutoScroll = true;
-      this->picture_box_panel_->Controls->Add(this->destination_picture_box_);
-      this->picture_box_panel_->Controls->Add(this->source_picture_box_);
       this->picture_box_panel_->Location = System::Drawing::Point(69, 63);
       this->picture_box_panel_->Name = L"picture_box_panel_";
       this->picture_box_panel_->Size = System::Drawing::Size(1190, 495);
       this->picture_box_panel_->TabIndex = 0;
-      // 
-      // destination_picture_box_
-      // 
-      this->destination_picture_box_->Location = System::Drawing::Point(111, 4);
-      this->destination_picture_box_->Name = L"destination_picture_box_";
-      this->destination_picture_box_->Size = System::Drawing::Size(100, 50);
-      this->destination_picture_box_->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
-      this->destination_picture_box_->TabIndex = 1;
-      this->destination_picture_box_->TabStop = false;
-      // 
-      // source_picture_box_
-      // 
-      this->source_picture_box_->Location = System::Drawing::Point(4, 4);
-      this->source_picture_box_->Name = L"source_picture_box_";
-      this->source_picture_box_->Size = System::Drawing::Size(100, 50);
-      this->source_picture_box_->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
-      this->source_picture_box_->TabIndex = 0;
-      this->source_picture_box_->TabStop = false;
       // 
       // files_menu_strip_
       // 
@@ -167,25 +137,18 @@ namespace ImageMorphing {
       // 
       // files_tool_strip_menu_item_
       // 
-      this->files_tool_strip_menu_item_->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
-        this->open_source_image_tool_strip_menu_item_,
-          this->open_destination_image_tool_strip_menu_item_
+      this->files_tool_strip_menu_item_->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {
+        this->add_image_tool_strip_menu_item_
       });
       this->files_tool_strip_menu_item_->Name = L"files_tool_strip_menu_item_";
       this->files_tool_strip_menu_item_->Size = System::Drawing::Size(42, 20);
       this->files_tool_strip_menu_item_->Text = L"Files";
       // 
-      // open_source_image_tool_strip_menu_item_
+      // add_image_tool_strip_menu_item_
       // 
-      this->open_source_image_tool_strip_menu_item_->Name = L"open_source_image_tool_strip_menu_item_";
-      this->open_source_image_tool_strip_menu_item_->Size = System::Drawing::Size(201, 22);
-      this->open_source_image_tool_strip_menu_item_->Text = L"Open source image";
-      // 
-      // open_destination_image_tool_strip_menu_item_
-      // 
-      this->open_destination_image_tool_strip_menu_item_->Name = L"open_destination_image_tool_strip_menu_item_";
-      this->open_destination_image_tool_strip_menu_item_->Size = System::Drawing::Size(201, 22);
-      this->open_destination_image_tool_strip_menu_item_->Text = L"Open destination image";
+      this->add_image_tool_strip_menu_item_->Name = L"add_image_tool_strip_menu_item_";
+      this->add_image_tool_strip_menu_item_->Size = System::Drawing::Size(152, 22);
+      this->add_image_tool_strip_menu_item_->Text = L"Add image";
       // 
       // start_button_
       // 
@@ -200,6 +163,9 @@ namespace ImageMorphing {
       // morphing_steps_numeric_up_down_
       // 
       this->morphing_steps_numeric_up_down_->Location = System::Drawing::Point(95, 30);
+      this->morphing_steps_numeric_up_down_->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) {
+        10000, 0, 0, 0
+      });
       this->morphing_steps_numeric_up_down_->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) {
         2, 0, 0, 0
       });
@@ -252,10 +218,6 @@ namespace ImageMorphing {
       this->MainMenuStrip = this->files_menu_strip_;
       this->Name = L"ApplicationForm";
       this->Text = L"ApplicationForm";
-      this->picture_box_panel_->ResumeLayout(false);
-      this->picture_box_panel_->PerformLayout();
-      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->destination_picture_box_))->EndInit();
-      (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->source_picture_box_))->EndInit();
       this->files_menu_strip_->ResumeLayout(false);
       this->files_menu_strip_->PerformLayout();
       (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->morphing_steps_numeric_up_down_))->EndInit();
